43236dde7af405a056c29b6eff6030e2e8d82e7e
Rogério Venâncio <rogeriogvmm@gmail.com>
2022-01-22 20:43:44 -0300

    feature: configuring the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { CityModule } from './city/city.module';
+import { StateModule } from './state/state.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ClientModule,
+    CityModule,
+    StateModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
