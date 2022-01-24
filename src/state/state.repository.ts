96a2bb6a423cffb1f88e756ad9276c841e132705
Rogério Venâncio <rogeriogvmm@gmail.com>
2022-01-24 18:14:02 -0300

    refactor: chage the exception when duplicate entry

--- src/state/state.repository.ts
+++ src/state/state.repository.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateStateDto } from './dto/create-state.dto';
 import { State } from './state.entity';
@@ -8,16 +13,17 @@ export class StateRepository extends Repository<State> {
   async createState(createStateDto: CreateStateDto): Promise<State> {
     const stateByName = await this.findOne(createStateDto.name);
 
-    if (stateByName) {
-      throw new BadRequestException(
+    if (stateByName)
+      throw new ConflictException(
         `State '${createStateDto.name}' already exist`,
       );
-    }
 
     const state = this.create(createStateDto);
-    await this.save(state);
-
-    return state;
+    const stateSaved = await this.save(state);
+    if (!stateSaved)
+      throw new InternalServerErrorException(`Couldn't create a State`);
+      
+    return stateSaved;
   }
 
   async findOneByName(name: string) {
